e7cc5657f2747ce25518b2f961408fd7175ccb5b
Esin Eddy Edet <edesin1989@gmail.com>
2023-10-24 16:59:36 +0100

    third

--- main.js
+++ main.js
@@ -241,6 +241,10 @@ closeBtn.addEventListener("click", function () {
 const formInput = document.querySelectorAll("input");
 const formT = document.querySelector(".form-textarea");
 const submitBtn = document.querySelector(".submit-btn");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const form = document.querySelector(".form");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -248,13 +252,80 @@ submitBtn.addEventListener("click", (e) => {
     input.value = "";
   });
   formT.value = "";
-  modal.classList.remove("open-modal");
+  // modal.classList.remove("open-modal");
 });
 
-// formInput.forEach((input) => (input.value = ""));
-// const form = document.querySelector(".form");
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   form.reset();
-// });
+// show input error message
+const showError = (input, message) => {
+  const formControl = input.parentElement;
+  formControl.className = "form-row error";
+  const small = formControl.querySelector("small");
+  console.log(small);
+  small.innerText = message;
+};
+// Show success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control success";
+}
+// Check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+// Check required fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+  return isRequired;
+}
+
+// Check input length
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  if (checkRequired([name, email, number, formT])) {
+    checkLength(name, 3, 15);
+    checkLength(formT, 6, 25);
+    checkLength(number, 6, 25);
+    checkEmail(email);
+  }
+});
+
+// initialize animation on scroll
 AOS.init();
